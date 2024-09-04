ace0cd1e2a253d51821db98ca8d8787d0d933002
Your Name <you@example.com>
2024-09-04 15:21:57 +0300

    render

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,9 +4,9 @@ const Cart = () => {
     return (
         <>
         <div>
-            <div><img src="" alt="" width="374px" height="374px"/></div>
+            <div><img src="#" alt="" width="374px" height="374px"/></div>
             <div>
-                <h2>назва піци</h2>
+                <h2>Pizza</h2>
                 <p>опис піци</p>
                 <p>60 л</p>
                 <button>замовити</button>
